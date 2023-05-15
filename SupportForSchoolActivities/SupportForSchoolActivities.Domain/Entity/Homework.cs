9e63a0f7dd88f568b7daea62a92897775fd312fe
Dmytro Obramenko <sobramenko@gmail.com>
2023-05-15 10:27:56 +0300

    Added models into entity folder

---
+++ SupportForSchoolActivities/SupportForSchoolActivities.Domain/Entity/Homework.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportForSchoolActivities.Domain.Entity
+{
+    public class Homework
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public virtual SchoolClass SchoolClass { get; set; }
+        public virtual Subject Subject { get; set; }
+    }
+}
---
+++ SupportForSchoolActivities/SupportForSchoolActivities.Domain/Entity/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportForSchoolActivities.Domain.Entity
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<Teacher> Teachers { get; set; }
+        public virtual ICollection<Grade> Grades { get; set; }
+        public virtual ICollection<Homework> Homeworks { get; set; }
+        public virtual ICollection<Schedule> Schedules { get; set; }
+    }
+}
