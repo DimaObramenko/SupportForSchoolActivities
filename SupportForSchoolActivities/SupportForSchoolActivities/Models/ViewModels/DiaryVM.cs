b4f10279bac07f3ba9b157f9f2e963170ba3026c
Dmytro Obramenko <sobramenko@gmail.com>
2023-06-13 12:44:24 +0300

    Configured diary

---
+++ SupportForSchoolActivities/SupportForSchoolActivities/Models/ViewModels/DiaryVM.cs
@@ -0,0 +1,13 @@
+﻿using SupportForSchoolActivities.Domain.Entity;
+
+namespace SupportForSchoolActivities.Models.ViewModels
+{
+    public class DiaryVM
+    {
+        public string StudentId { get; set; }
+        public List<Schedule> Schedules { get; set; }
+        public List<Grade> Grades { get; set; }
+        public List<Homework> Homeworks { get; set; }
+        public List<DateTime> Dates { get; set; }
+    }
+}
