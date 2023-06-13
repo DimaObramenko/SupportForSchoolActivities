01f630ecd82e2cb8da892180313327126132fc5f
Dmytro Obramenko <sobramenko@gmail.com>
2023-06-13 09:44:12 +0300

    Added student progress table

---
+++ SupportForSchoolActivities/SupportForSchoolActivities/Models/ViewModels/StudentProgressVM.cs
@@ -0,0 +1,13 @@
+﻿using SupportForSchoolActivities.Domain.Entity;
+
+namespace SupportForSchoolActivities.Models.ViewModels
+{
+    public class StudentProgressVM
+    {
+        public Student Student { get; set; }
+        public List<DateTime> DateList { get; set; }
+        public List<Subject> Subjects { get; set; }
+        public List<Grade> Grades { get; set; }
+
+    }
+}
